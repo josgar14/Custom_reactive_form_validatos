02fb747cedd6989157ee21653baef872ba2a433c
Josue <josgar_14@hotmail.com>
2019-09-29 18:31:58 -0400

    custom reactive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ValidationErrors } from '@angular/forms';
 
+function ssnValidator(control: FormControl): ValidationErrors | null {
+	const value: string = control.value || '';
+	const valid = value.match(/^\d{9}$/);
+	return valid ? null : { ssn: { description: 'SSN is invalid' } };
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'CustomFormValidator';
+	myForm: FormGroup;
+
+	constructor() {
+		this.myForm = new FormGroup({
+			socialSecurity: new FormControl('', ssnValidator)
+		});
+	}
 }
